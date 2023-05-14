0e068fa0939fed51e9236f3c989df1d57509c7f2
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-05-14 20:29:25 +1200

    Fixing script errors

--- provisioning.sh
+++ provisioning.sh
@@ -15,41 +15,41 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 echo "Creating users ..."
-sudo useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+sudo useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_ADM
 sudo passwd carlos -e
-sudo useradd debora  -c "Debora" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+sudo useradd debora  -c "Debora" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_ADM
 sudo passwd debora -e
-sudo useradd josefina  -c "Josefina" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+sudo useradd josefina  -c "Josefina" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_ADM
 sudo passwd josefina -e
 
-sudo useradd maria -c "Josefina" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+sudo useradd maria -c "Josefina" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_VEN
 sudo passwd maria -e
-sudo useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+sudo useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_VEN
 sudo passwd sebastiana -e
-sudo useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+sudo useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_VEN
 sudo passwd amanda -e
 
-sudo useradd joao -c "Joao" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+sudo useradd joao -c "Joao" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_SEC
 sudo passwd joao -e
-sudo useradd roberto -c "Roberto" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+sudo useradd roberto -c "Roberto" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_SEC
 sudo passwd roberto -e
-sudo useradd rogerio -c "Rogerio" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+sudo useradd rogerio -c "Rogerio" -m -s /bin/bash -p $(openssl passwd Senha123) -g GRP_SEC
 sudo passwd rogerio -e
 
 
-echo "Adding users to it respective groups..."
-sudo usermod -g GRP_ADM carlos
-sudo usermod -g GRP_ADM debora
-sudo usermod -g GRP_ADM josefina
-sudo usermod -g GRP_VEN maria
-sudo usermod -g GRP_ADM sebastiana
-sudo usermod -g GRP_ADM amanda
-sudo usermod -g GRP_SEC joao
-sudo usermod -g GRP_ADM roberto
-sudo usermod -g GRP_ADM rogerio
+echo "Adding users to its respective groups..."
+#sudo usermod -G GRP_ADM carlos
+#sudo usermod -G GRP_ADM debora
+#sudo usermod -G GRP_ADM josefina
+#sudo usermod -G GRP_VEN maria
+#sudo usermod -G GRP_ADM sebastiana
+#sudo usermod -G GRP_ADM amanda
+#sudo usermod -G GRP_SEC joao
+#sudo usermod -G GRP_ADM roberto
+#sudo usermod -G GRP_ADM rogerio
 
 #All users will have full permissions on the public directory
-sudo chmod o=wrx /public
+sudo chmod o+wrx /public
 
 #Users of each group will have full permissions within the respective directory 
 #referring to the department they are part of
@@ -62,5 +62,5 @@ sudo chmod o-wrx /adm
 sudo chmod o-wrx /ven
 sudo chmod o-wrx /sec
 
-echo ('Fixing “passwd: Authentication token manipulation error”')
+echo "Fixing passwd: Authentication token manipulation error"
 sudo reboot
