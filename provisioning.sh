e45b6f21bc6b309e435955cc7dcae29d05761646
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-05-14 14:47:21 +1200

    Giving permissions for group in the respective directory

--- provisioning.sh
+++ provisioning.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 echo "Creating directories ..."
-
 mkdir /public /adm /ven /sec
 
-echo "Creating groups ..."
+echo"The user root will be the owner of all created directories"
+chown root /public
+chown root /adm 
+chown root /ven
+chown root /sec
 
+echo "Creating groups ..."
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
 echo "Creating users ..."
-
 useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
 useradd debora  -c "Debora" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
 useradd josefina  -c "Josefina" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
@@ -29,3 +32,18 @@ echo "Adding users to it respective groups..."
 usermod -G GRP_ADM carlos, debora, josefina
 usermod -G GRP_VEN maria, sebastiana, amanda
 usermod -G GRP_SEC joao, roberto, rogerio
+
+#All users will have full permissions on the public directory
+chmod o=rx /public
+
+#Users of each group will have full permissions within the respective directory 
+#referring to the department they are part of
+chmod g+wrx /adm
+chmod g+wrx /ven
+chmod g+wrx /sec
+
+#The user cannot read, write or execute permissions on directories of departments that they do not belong to
+chmod o-wrx /adm
+chmod o-wrx /ven
+chmod o-wrx /sec
+
