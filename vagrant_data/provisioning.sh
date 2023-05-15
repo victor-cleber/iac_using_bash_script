3a9a025853a2b965af0dad544da47ea3e8cc8e6f
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-05-15 14:20:12 +1200

    Adding more detailed echo messages

--- vagrant_data/provisioning.sh
+++ vagrant_data/provisioning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Creating directories ..."
+echo "Creating directories..."
 mkdir /public /adm /ven /sec
 
 #echo"The user root will be the owner of all created directories"
@@ -19,43 +19,45 @@ chown :GRP_ADM  /adm
 chown :GRP_VEN /ven
 chown :GRP_SEC /sec
 
-echo "Creating users ..."
+echo "Creating users..."
 useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd Senha123)
 usermod -G GRP_ADM carlos
-passwd carlos -e
+passwd carlos -e > /dev/null
 useradd debora  -c "Debora" -m -s /bin/bash -p $(openssl passwd Senha123)
 usermod -G GRP_ADM debora
-passwd debora -e
+passwd debora -e > /dev/null
 useradd josefina  -c "Josefina" -m -s /bin/bash -p $(openssl passwd Senha123)
 usermod -G GRP_ADM josefina
-passwd josefina -e
+passwd josefina -e > /dev/null
 
 useradd maria -c "Maria" -m -s /bin/bash -p $(openssl passwd Senha123)
 usermod -G GRP_VEN maria
-passwd maria -e
+passwd maria -e > /dev/null
 useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd Senha123) 
 usermod -G GRP_VEN sebastiana
-passwd sebastiana -e
+passwd sebastiana -e > /dev/null
 useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd Senha123)
 usermod -G GRP_VEN amanda
-passwd amanda -e
+passwd amanda -e > /dev/null
 
 useradd joao -c "Joao" -m -s /bin/bash -p $(openssl passwd Senha123)
 usermod -G GRP_SEC joao
-passwd joao -e
+passwd joao -e > /dev/null
 useradd roberto -c "Roberto" -m -s /bin/bash -p $(openssl passwd Senha123)
 usermod -G GRP_SEC roberto
-passwd roberto -e
+passwd roberto -e > /dev/null
 useradd rogerio -c "Rogerio" -m -s /bin/bash -p $(openssl passwd Senha123)
 usermod -G GRP_SEC rogerio
-passwd rogerio -e
+passwd rogerio -e > /dev/null
 
 
 #All users will have full permissions on the public directory
+echo "Applying permissions into the public directory..."
 chmod o+wrx /public
 
 #Users of each group will have full permissions within the respective directory 
 #referring to the department they are part of
+echo "Applying permissions into the groups..."
 chmod g+wrx /adm
 chmod g+wrx /ven
 chmod g+wrx /sec
@@ -65,5 +67,5 @@ chmod o-wrx /adm
 chmod o-wrx /ven
 chmod o-wrx /sec
 
-echo "Rebooting to fix "passwd: Authentication token manipulation" error"
+echo "Rebooting to fix "passwd: Authentication token manipulation" error"...
 reboot
