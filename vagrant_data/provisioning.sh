a74947989ae5ebc6d2ee226d36e9ada8fecb2fe4
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-05-15 13:44:46 +1200

    Changing directory group owner

--- vagrant_data/provisioning.sh
+++ vagrant_data/provisioning.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating directories ..."
-sudo mkdir /public /adm /ven /sec
+mkdir /public /adm /ven /sec
 
 #echo"The user root will be the owner of all created directories"
 #chown root /public
@@ -14,51 +14,55 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
+chown :GRP_ADM  /adm
+chown :GRP_VEN /ven
+chown :GRP_SEC /sec
+
 echo "Creating users ..."
-sudo useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd Senha123)
-sudo usermod -G GRP_ADM carlos
-sudo passwd carlos -e
-sudo useradd debora  -c "Debora" -m -s /bin/bash -p $(openssl passwd Senha123)
-sudo usermod -G GRP_ADM debora
-sudo passwd debora -e
-sudo useradd josefina  -c "Josefina" -m -s /bin/bash -p $(openssl passwd Senha123)
-sudo usermod -G GRP_ADM josefina
-sudo passwd josefina -e
+useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd Senha123)
+usermod -G GRP_ADM carlos
+passwd carlos -e
+useradd debora  -c "Debora" -m -s /bin/bash -p $(openssl passwd Senha123)
+usermod -G GRP_ADM debora
+passwd debora -e
+useradd josefina  -c "Josefina" -m -s /bin/bash -p $(openssl passwd Senha123)
+usermod -G GRP_ADM josefina
+passwd josefina -e
 
-sudo useradd maria -c "Maria" -m -s /bin/bash -p $(openssl passwd Senha123)
-sudo usermod -G GRP_VEN maria
-sudo passwd maria -e
-sudo useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd Senha123) 
-sudo usermod -G GRP_VEN sebastiana
-sudo passwd sebastiana -e
-sudo useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd Senha123)
-sudo usermod -G GRP_VEN amanda
-sudo passwd amanda -e
+useradd maria -c "Maria" -m -s /bin/bash -p $(openssl passwd Senha123)
+usermod -G GRP_VEN maria
+passwd maria -e
+useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd Senha123) 
+usermod -G GRP_VEN sebastiana
+passwd sebastiana -e
+useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd Senha123)
+usermod -G GRP_VEN amanda
+passwd amanda -e
 
-sudo useradd joao -c "Joao" -m -s /bin/bash -p $(openssl passwd Senha123)
-sudo usermod -G GRP_SEC joao
-sudo passwd joao -e
-sudo useradd roberto -c "Roberto" -m -s /bin/bash -p $(openssl passwd Senha123)
-sudo usermod -G GRP_SEC roberto
-sudo passwd roberto -e
-sudo useradd rogerio -c "Rogerio" -m -s /bin/bash -p $(openssl passwd Senha123)
-sudo usermod -G GRP_SEC rogerio
-sudo passwd rogerio -e
+useradd joao -c "Joao" -m -s /bin/bash -p $(openssl passwd Senha123)
+usermod -G GRP_SEC joao
+passwd joao -e
+useradd roberto -c "Roberto" -m -s /bin/bash -p $(openssl passwd Senha123)
+usermod -G GRP_SEC roberto
+passwd roberto -e
+useradd rogerio -c "Rogerio" -m -s /bin/bash -p $(openssl passwd Senha123)
+usermod -G GRP_SEC rogerio
+passwd rogerio -e
 
 
 #All users will have full permissions on the public directory
-sudo chmod o+wrx /public
+chmod o+wrx /public
 
 #Users of each group will have full permissions within the respective directory 
 #referring to the department they are part of
-sudo chmod g+wrx /adm
-sudo chmod g+wrx /ven
-sudo chmod g+wrx /sec
+chmod g+wrx /adm
+chmod g+wrx /ven
+chmod g+wrx /sec
 
 #The user cannot read, write or execute permissions on directories of departments that they do not belong to
-sudo chmod o-wrx /adm
-sudo chmod o-wrx /ven
-sudo chmod o-wrx /sec
+chmod o-wrx /adm
+chmod o-wrx /ven
+chmod o-wrx /sec
 
-echo "Reboot to fix passwd: Authentication token manipulation error"
-sudo reboot
+echo "Rebooting to fix "passwd: Authentication token manipulation" error"
+reboot
